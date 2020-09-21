b530551ba93ead727c3a7f56ba42b33dece48356
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-21 22:54:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,16 +4,16 @@
 def key_for_min_value(name_hash)
  #a=[]
 # b=[]
-# i=0
+ #i=0
   name_hash.each do |key, value|
   #  a=value #storing the values in an empty array
   #  b=key # storing the keys in an empty array
   #  while i<length.name_hash do
 
     if value<value+1
-
+    #  i=value
       return key
-      value +=1
+      
 
     elsif value>value+1
 
