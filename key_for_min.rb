5a9da62ce277996b782dc38a6edfe8be3ac62cf0
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-21 23:17:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
    a=value #storing the values in an empty array
    b=key # storing the keys in an empty array
    while i<name_hash.count do
-    
+
     if a[i]<a[i+1]
 
       return b
@@ -18,7 +18,7 @@ def key_for_min_value(name_hash)
     elsif name_hash==nil
       return nil
     end #ends the if elseif block
-  
+
   end #ends the while do block
     i +=1
 value +=1 #increment the value for conditional statement
