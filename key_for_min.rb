a6b900cf1936ae1e3ebf1dead7eb130952849661
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-21 22:45:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-# s={}
+ #a=[]
+# b=[]
+# i=0
   name_hash.each do |key, value|
+  #  a=value #storing the values in an empty array
+  #  b=key # storing the keys in an empty array
+  #  while i<length.name_hash do
     if value<value+1
+
       return key
-  #    return s
-    else
-      return key+1
+      value +=1
     end
+  #    return s
+  #  else
+  #    return key+1
+  #  end
 
 value +=1
 
