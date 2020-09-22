a5bd983d992ab1ca43965664bbe8f50c1ebe460c
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-22 00:54:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 #require 'pry'
 def key_for_min_value(name_hash)
- a=[]
  b={}
  i=1000
   name_hash.each do |key, value|
    if value<i
    i=value
 end
-name_hash.select{|key,value| value==i}
+name_hash.key(i)
 
   end #ends the do block
 
