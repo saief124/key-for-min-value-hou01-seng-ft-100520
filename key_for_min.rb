75aa291157fd219c6c6efb80803c1f644b4996d4
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-22 00:34:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,18 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-require 'pry'
+#require 'pry'
 def key_for_min_value(name_hash)
  a=[]
  b={}
  i=1000
   name_hash.each do |key, value|
-   a=value #storing the values in an empty array
-   b=key # storing the keys in an empty hash
+   if value<i #storing the values in an empty array
+   i=value # storing the keys in an empty hash
 
   end #ends the do block
- return a
- return b
- binding.pry
+
+ return i
+# binding.pry
 end #ends the method
 
 puts "Hello World"
