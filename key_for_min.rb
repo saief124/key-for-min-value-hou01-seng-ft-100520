66077ba5c35a487a631785f7e3843b0422a61e69
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-22 00:58:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,13 @@ def key_for_min_value(name_hash)
  b={}
  i=1000
   name_hash.each do |key, value|
-   if value<i
-   i=value
+   if value<i #comparing first value to 1000
+   i=value #setting variable i to the value, so it can compare with the value in next iteration
 end
 
 
   end #ends the do block
 
-name_hash.key(i)
+name_hash.key(i) #gets the key associated with the value found out in the do block above, returns nil if no value found
 
 end #ends the method
