f6736a034ee695682d17f88c080e0be386c3ae50
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-22 00:24:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,15 +9,10 @@ def key_for_min_value(name_hash)
    a=value #storing the values in an empty array
    b=key # storing the keys in an empty hash
 
-
-
-
-
-
-
-
-
-
   end #ends the do block
+ return a
+ return b
  binding.pry
 end #ends the method
+
+puts "Hello World"
