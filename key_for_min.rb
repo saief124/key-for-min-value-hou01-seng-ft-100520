48532ff5b59b6b3d9d6f240b4269e63907b92fad
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-22 00:20:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,19 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 #require 'pry'
 def key_for_min_value(name_hash)
- #a=[]
+ a=[]
  b={}
  i=1000
   name_hash.each do |key, value|
-#   a=value #storing the values in an empty array
-#   b=key # storing the keys in an empty array
-  # while i<name_hash.count do
-
-puts key
-puts value
+   a=value #storing the values in an empty array
+   b=key # storing the keys in an empty hash
 
 
-#  end #ends the while do block
+
+
+
+
 
 
 
