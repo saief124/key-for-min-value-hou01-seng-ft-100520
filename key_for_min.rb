8fa2a4fd41c72fb7c113826175e201e02c03f322
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-22 00:10:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-require 'pry'
+#require 'pry'
 def key_for_min_value(name_hash)
  #a=[]
  b={}
@@ -20,5 +20,5 @@ puts value
 
 
   end #ends the do block
-  binding.pry
+#  binding.pry
 end #ends the method
